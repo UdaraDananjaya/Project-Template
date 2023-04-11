3a5e1b2dc5a88acfabf1afcfb798e3dc346ba16c
Udara Dananjaya <budaththanayaka@gmail.com>
2023-04-12 00:01:38 +0530

    Revert "Revert "Update Table Protected Variable""
    This reverts commit 1c798a20e9fcf5d692e261c922c6d55e4d5d49a0.

--- App/models/Admin_model.php
+++ App/models/Admin_model.php
@@ -1,22 +1,47 @@
-<?php 
+<?php
+
 /**
  * Admin model class
  */
 class Admin_model
 {
-	
+
 	use Model;
 
-	protected $table ;
+	protected $limit = 10;
+	protected $offset = 0;
+	protected $order_type = "asc";
+	protected $order_column = "id";
+	protected $table;
 	protected $allowedColumns = [];
 
-	function set_table($name) {
+	function set_table($name)
+	{
 		$this->table = $name;
 	}
 
-	function set_allowedColumns($allowedColumns) {
+	function set_allowedColumns($allowedColumns)
+	{
 		$this->allowedColumns = $allowedColumns;
 	}
 
+	function set_order_type($order_type)
+	{
+		$this->order_type = $order_type;
+	}
 
+	function set_order_column($order_column)
+	{
+		$this->order_column = $order_column;
+	}
+
+	function set_limit($limit)
+	{
+		$this->limit = $limit;
+	}
+
+	function set_offset($offset)
+	{
+		$this->offset = $offset;
+	}
 }
